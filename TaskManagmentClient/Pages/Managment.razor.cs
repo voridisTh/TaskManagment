a495601799df5ad30022bd63399fec99525a267a
TheoVoridis <Voridis@DESKTOP-GO6VGBC>
2025-07-03 11:14:21 +0300

    New Managment Page

---
+++ TaskManagmentClient/Pages/Managment.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+
+namespace TaskManagmentClient.Pages
+{
+    public class ManagmentBase : ComponentBase
+    {
+        [Inject] public NavigationManager Nav { get; set; } = default!;
+        [Inject] protected HttpClient Http { get; set; }
+        protected List<ProjectDto> Projects { get; set; } = new();
+
+        protected List<StatusDto> statuses = new();
+        protected bool IsBusy { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            IsBusy = true;
+            Projects = await Http.GetFromJsonAsync<List<ProjectDto>>("api/TicketManagment/projects");
+            statuses = await Http.GetFromJsonAsync<List<StatusDto>>("api/TicketManagment/statuses");
+            IsBusy = false;
+        }
+
+        protected void NavToEdit(int id)
+            => Nav.NavigateTo($"/projects/edit/{id}");
+    }
+}
